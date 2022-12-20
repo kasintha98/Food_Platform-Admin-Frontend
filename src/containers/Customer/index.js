d1faea3db16de7ea8615e50e31a99ba728cc74f4
Rahul Deshmukh <rahul.deshmukh2@tcs.com>
2022-12-21 00:47:16 +0530

    Customer Data Added and Preset and store removed for Cunsumption and cancel Order reports

--- src/containers/Customer/index.js
+++ src/containers/Customer/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import Layout from "../NewLayout";
 
 export const Customer = () => {
+
+  var htmlCustomerDataIframe = '<iframe width="100%" height="800" src="https://datastudio.google.com/embed/reporting/5c92ed2a-3233-41e0-8848-470f4d277654/page/w679C" frameborder="0" style="border:0" allowfullscreen></iframe>';
+
   return (
     <Layout sidebar headerTitle="Customer">
-      <div>Customer</div>
+      <div style={{maxWidth:'100%',maxHeight:'1068px',minWidth:800}}>
+        <div className="content" dangerouslySetInnerHTML={{__html: htmlCustomerDataIframe}}></div>
+      </div>
     </Layout>
   );
 };
